4f8d4a4b9096487ec8eddf02f10e362506e5c10d
Prathmesh <connect.prathmesh905@gmail.com>
2022-09-30 15:52:02 +0530

    JS is all working and it is responsive

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ function addition () {
     result = Number(t1) + Number(t2);
     document.getElementById("output").innerHTML=result;
 }
+
+function subtraction () {
+    let t1=document.getElementById("num1").value;
+    let t2=document.getElementById("num2").value;
+    result = Number(t1) - Number(t2);
+    document.getElementById("output").innerHTML=result;
+}
+function multiplication () {
+    let t1=document.getElementById("num1").value;
+    let t2=document.getElementById("num2").value;
+    result = Number(t1) * Number(t2);
+    document.getElementById("output").innerHTML=result;
+}
+function division () {
+    let t1=document.getElementById("num1").value;
+    let t2=document.getElementById("num2").value;
+    result = Number(t1) / Number(t2);
+    document.getElementById("output").innerHTML=result;
+}
+function modulas () {
+    let t1=document.getElementById("num1").value;
+    let t2=document.getElementById("num2").value;
+    result = Number(t1) % Number(t2);
+    document.getElementById("output").innerHTML=result;
+}
